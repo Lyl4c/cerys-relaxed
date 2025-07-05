29100e334208b0f3c0043027fd6cfe4820e984b8
Lylac_SYS <7102149+Lyl4c@users.noreply.github.com>
2025-07-05 11:03:40 -0700

    moved to version 1.1.4

--- prototypes/override-final-final/recipe.lua
+++ prototypes/override-final-final/recipe.lua
@@ -5,6 +5,6 @@ data.raw["recipe"]["cerys-charging-rod"].additional_categories = {
 data.raw["recipe"]["cerys-charging-rod"].ingredients = {
   { type = "item", name = "superconductor", amount = 8 },
   { type = "item", name = "steel-plate", amount = 8 },
-  { type = "item", name = "holmium-plate", amount = 8 }, -- 16.5 holmium plates per charging rod?? really?!
+  { type = "item", name = "holmium-plate", amount = 8 }, -- 20 holmium plates per charging rod?? really?!
   { type = "item", name = "copper-cable", amount = 20}
 }
